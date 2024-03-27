18c13a161e901ab97958f003bc3d17ad7d33942e
Jelinskyy <aleksanderjs12@gmail.com>
2024-03-27 22:29:26 +0100

    Added storing tags in db after fetch

--- API/Controllers/TagController.cs
+++ API/Controllers/TagController.cs
@@ -1,9 +1,8 @@
 ﻿using System.Net;
-using System.Text.Json;
-using System.Text.Json.Nodes;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
 namespace API.Controllers;
@@ -23,31 +22,72 @@ public class TagController : ControllerBase
             });
     }
 
-    public partial class SOApiRespons{
-        public List<Tag>? tags { get; set; }
-        public bool hasMore {get; set; }
-        public int quotaMax {get; set; }
-        public int quotaRemaining {get; set; }
-    }
-
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Tag>>> GetTags()
     {
-        string apiUrl = "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"; 
-        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+        ActionResult<IEnumerable<Tag>> tags;
 
-        if (response.IsSuccessStatusCode)
-        {
+        // Check if there are tags in db
+        if(_context.Tags.Count() <= 0) {
+            // if no do fetch request
+            tags = await FetchTags();
+        }else {
+            // if yes get them from db
+            tags = await _context.Tags.ToListAsync();
+        }
+        
+        return tags;
+    }
+
+    public partial class SOApiRespons{
+        [JsonProperty("items")]
+        public List<Tag>? tags { get; set; }
+        [JsonProperty("has_more")]
+        public bool hasMore {get; set; }
+        [JsonProperty("quota_max")]
+        public int quotaMax {get; set; }
+        [JsonProperty("quota_remaining")]
+        public int quotaRemaining {get; set; }
+    }
+
+    [HttpGet("fetch")]
+    public async Task<ActionResult<IEnumerable<Tag>>> FetchTags()
+    {
+        string apiUrl = "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow&pagesize=100"; 
+
+        //page number
+        int pageNumber = 1;
+
+        //all tags list
+        List<Tag> tags = new List<Tag>();
+
+        do{
+            // Send request to api
+            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl+"&page="+pageNumber);
+            if (!response.IsSuccessStatusCode){
+                // Handle the error condition
+                return StatusCode((int)response.StatusCode);
+            }
+
+            // Deserializing data
             var data = await response.Content.ReadAsStringAsync();
-            List<Tag>? deserialized = JsonConvert.DeserializeObject<List<Tag>>(data);
-            
-            // Process the data or return it as-is
-            return Ok(deserialized);
-        }
-        else
-        {
-            // Handle the error condition
-            return StatusCode((int)response.StatusCode);
-        }
+            var deserialized = JsonConvert.DeserializeObject<SOApiRespons>(data);
+
+            // Adding data to table
+            tags.AddRange(deserialized.tags);
+
+            //braking loop if there is no next page
+            if(deserialized.hasMore == false) 
+            {
+                break;
+            }
+
+            pageNumber++;
+        }while (tags.Count < 100);
+
+        await _context.Tags.AddRangeAsync(tags);
+        await _context.SaveChangesAsync();
+        
+        return Ok(tags);
     }
 }
--- API/Entities/Tag.cs
+++ API/Entities/Tag.cs
@@ -1,11 +1,15 @@
-﻿namespace API.Entities;
+﻿using Newtonsoft.Json;
+
+namespace API.Entities;
 
 public class Tag
 {
     public int Id { get; set; }
-
+    [JsonProperty("has_synonyms")]
     public bool HasSynonyms { get; set; }
+    [JsonProperty("is_moderator_only")]
     public bool IsMadatorOnly { get; set; }
+    [JsonProperty("is_required")]
     public bool IsRequired { get; set; }
 
     public int Count { get; set; }
