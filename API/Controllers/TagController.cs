ee289dbb32e4b02b4f29e489c9d1cfa5843d9232
Jelinskyy <aleksanderjs12@gmail.com>
2024-03-27 15:47:57 +0100

    Added Tags Controller

---
+++ API/Controllers/TagController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class TagController : ControllerBase
+{
+    private readonly DataContext _context;
+    public TagController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Tag>> GetUsers()
+    {
+        var tags = _context.Tags.ToList();
+
+        return tags;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Tag?> GetUsers(int id)
+    {
+        return _context.Tags.Find(id);
+    }
+}
