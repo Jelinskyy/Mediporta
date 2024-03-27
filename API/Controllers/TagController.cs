ead57b5c22058b030b5a4ae5443fbe0c61337745
Jelinskyy <aleksanderjs12@gmail.com>
2024-03-27 18:51:41 +0100

    Handled SO api requests in TagController

--- API/Controllers/TagController.cs
+++ API/Controllers/TagController.cs
@@ -1,6 +1,10 @@
-﻿using API.Data;
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace API.Controllers;
 
@@ -9,22 +13,41 @@ namespace API.Controllers;
 public class TagController : ControllerBase
 {
     private readonly DataContext _context;
+    private readonly HttpClient _httpClient;
     public TagController(DataContext context)
     {
         _context = context;
+        _httpClient = new HttpClient(new HttpClientHandler
+            { 
+                AutomaticDecompression = DecompressionMethods.GZip
+            });
+    }
+
+    public partial class SOApiRespons{
+        public List<Tag>? tags { get; set; }
+        public bool hasMore {get; set; }
+        public int quotaMax {get; set; }
+        public int quotaRemaining {get; set; }
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<Tag>> GetUsers()
+    public async Task<ActionResult<IEnumerable<Tag>>> GetTags()
     {
-        var tags = _context.Tags.ToList();
+        string apiUrl = "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"; 
+        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
 
-        return tags;
-    }
-
-    [HttpGet("{id}")]
-    public ActionResult<Tag?> GetUsers(int id)
-    {
-        return _context.Tags.Find(id);
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadAsStringAsync();
+            List<Tag>? deserialized = JsonConvert.DeserializeObject<List<Tag>>(data);
+            
+            // Process the data or return it as-is
+            return Ok(deserialized);
+        }
+        else
+        {
+            // Handle the error condition
+            return StatusCode((int)response.StatusCode);
+        }
     }
 }
