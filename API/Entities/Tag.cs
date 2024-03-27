8a3c9e96cb7443a4005b81f10ad63eea3376a303
Jelinskyy <aleksanderjs12@gmail.com>
2024-03-27 14:54:08 +0100

    Created Tag Entity

---
+++ API/Entities/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace API;
+
+public class Tag
+{
+    public int Id { get; set; }
+
+    public bool HasSynonyms { get; set; }
+    public bool IsMadatorOnly { get; set; }
+    public bool IsRequired { get; set; }
+
+    public int Count { get; set; }
+
+    public string? Name { get ; set; }
+}
